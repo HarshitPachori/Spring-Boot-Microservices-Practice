d142d0067b41911b8240d7757c54238fc14b7c0a
HarshitPachori <harshitpachori345@gmail.com>
2026-07-09 22:29:16 +0530

    (feat) : created order and orderline repository to interact with postgresql database

---
+++ services/order-service/src/main/java/com/practice/order_service/orderline/OrderLineRepository.java
@@ -0,0 +1,7 @@
+package com.practice.order_service.orderline;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderLineRepository extends JpaRepository<OrderLine, Integer> {
+
+}
