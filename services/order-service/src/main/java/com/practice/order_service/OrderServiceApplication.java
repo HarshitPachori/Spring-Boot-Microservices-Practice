d72af9dbedbfe8244148b0ebe143335facbbbafa
HarshitPachori <harshitpachori345@gmail.com>
2026-07-12 14:07:46 +0530

    (feat) : created order line repository and added customer feign client

--- services/order-service/src/main/java/com/practice/order_service/OrderServiceApplication.java
+++ services/order-service/src/main/java/com/practice/order_service/OrderServiceApplication.java
@@ -2,8 +2,12 @@ package com.practice.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableJpaAuditing // because we use jpa listener
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- services/order-service/src/main/java/com/practice/order_service/orderline/OrderLineRepository.java
+++ services/order-service/src/main/java/com/practice/order_service/orderline/OrderLineRepository.java
@@ -1,7 +1,11 @@
 package com.practice.order_service.orderline;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderLineRepository extends JpaRepository<OrderLine, Integer> {
+
+  List<OrderLine> findAllByOrderId(Integer orderId);
 
 }
